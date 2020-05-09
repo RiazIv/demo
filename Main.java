f853efe34e649baa240a65d2660cd5f239963abd
RiazIv <64858334+RiazIv@users.noreply.github.com>
2020-05-09 16:03:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+//editing our project on github
 package com.demo;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 7, b = 6, c;
+        c = (a+b)*b;
     }
 }
