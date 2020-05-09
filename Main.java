a31f8a58e4c66b5b81716e295fcd3fef3ef4090b
Riazantsev Ivan <ryazantsev.ivan.e@yandex.ru>
2020-05-09 15:31:12 +0300

    first commit branch release

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-// this branch develop
+// this branch release
 package com.demo;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println();
-        System.out.println("Branch develop");
+        System.out.println("--------------");
+        System.out.println("Branch release");
     }
 }
