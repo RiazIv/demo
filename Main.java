8753bd8861704c673a19d67d28b3dc07ae448e30
Riazantsev Ivan <ryazantsev.ivan.e@yandex.ru>
2020-05-09 15:42:11 +0300

    add arifmetic operation

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println();
-        System.out.println("Branch develop");
+        int a=5, b=3, c = 0;
+        c = a +b;
+        System.out.println("Результат: " + c);
     }
 }
